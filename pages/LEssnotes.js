878cbbf5eca03529ca5d2ed145a3659d08181fa9
Roel <roel.mast90@gmail.com>
2019-02-13 14:13:44 +0100

    Set up js files and declare submitComment

---
+++ pages/LEssnotes.js
@@ -0,0 +1,10 @@
+const teacher = {
+    name: "Dan",
+    age: 23,
+    callStudent(student) {
+        console.log('Hello => ${$student}')
+    }
+}
+teacher.callStudent("Roel")
+teacher.age = 29
+
